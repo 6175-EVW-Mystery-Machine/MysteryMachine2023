5274f2b8f3be61d4da475d6766af119eae267880
6175-EVW-Mystery-Machine <evwrobotics@evw.k12.mn.us>
2023-03-01 13:43:25 -0600

    DriveUpChanges(NOT TESTED)

--- src/main/java/frc/robot/commands/AutoBalanceSeq2.java
+++ src/main/java/frc/robot/commands/AutoBalanceSeq2.java
@@ -6,19 +6,26 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Tram;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoBalance extends SequentialCommandGroup {
+public class AutoBalanceSeq2 extends SequentialCommandGroup {
   /** Creates a new AutoBalance. */
-  public AutoBalance(DriveTrain m_drivetrain){
+  public AutoBalanceSeq2(DriveTrain m_drivetrain, Tram m_tram, Arm m_arm, Claw m_claw){
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new WaitCommand(8),
+    new CubeExtend(m_arm, m_claw, m_tram),
+    new DriveForDistance(25, 0.4, 0, m_drivetrain),
+    new OpenClaw(m_claw),
 
+    new WaitCommand(1),
+// BALANCE FROM AGAINST POST
       new DriveForDistance(86, -0.4, 0, m_drivetrain),
       new WaitCommand(0.25),
       new DriveForDistance(3.5, 0.4, 0, m_drivetrain),
@@ -28,7 +35,7 @@ public class AutoBalance extends SequentialCommandGroup {
      new DriveRotateToSetpoint(270, m_drivetrain),
     //  new DriveWithJoy(() ->-0.1, ()->0, m_drivetrain).withTimeout(0.75),
 
-     new ClapSequence(null)
+     new ClapSequence(m_claw)
 
     );
   }
--- src/main/java/frc/robot/commands/ClapSequence.java
+++ src/main/java/frc/robot/commands/ClapSequence.java
@@ -6,29 +6,36 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.subsystems.DriveTrain;
+// import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Claw;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoBalance extends SequentialCommandGroup {
-  /** Creates a new AutoBalance. */
-  public AutoBalance(DriveTrain m_drivetrain){
+public class ClapSequence extends SequentialCommandGroup {
+  /** Creates a new ClapSequence. */
+  public ClapSequence(Claw m_claw) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new WaitCommand(8),
 
-      new DriveForDistance(86, -0.4, 0, m_drivetrain),
-      new WaitCommand(0.25),
-      new DriveForDistance(3.5, 0.4, 0, m_drivetrain),
-      new DriveForDistance(4, -0.3, 0, m_drivetrain),
-      new DriveToggleBreakMode(m_drivetrain),
-      new WaitCommand(0.5),
-     new DriveRotateToSetpoint(270, m_drivetrain),
-    //  new DriveWithJoy(() ->-0.1, ()->0, m_drivetrain).withTimeout(0.75),
-
-     new ClapSequence(null)
+    new CloseClaw(m_claw),
+    new WaitCommand(0.125),
+    new OpenClaw(m_claw),
+    new WaitCommand(0.125),
+    new CloseClaw(m_claw),
+    new WaitCommand(0.125),
+    new OpenClaw(m_claw),
+    new WaitCommand(0.125),
+    new CloseClaw(m_claw),
+    new WaitCommand(0.125),
+    new OpenClaw(m_claw),
+    new WaitCommand(0.125),
+    new CloseClaw(m_claw),
+    new WaitCommand(0.125),
+    new OpenClaw(m_claw),
+    new WaitCommand(0.125),
+    new CloseClaw(m_claw)
 
     );
   }
