b9f407e712c540dd26114f6a1ed0d7e27d89dbfb
6175-EVW-Mystery-Machine <evwrobotics@evw.k12.mn.us>
2023-03-01 20:50:55 -0600

    Removed Unused Imports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,11 +16,9 @@ import edu.wpi.first.hal.FRCNetComm.tInstances;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.hal.HAL;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 /**
  * The VM is configured to automatically run this class, and to call the
--- src/main/java/frc/robot/commands/ToggleClaw.java
+++ src/main/java/frc/robot/commands/ToggleClaw.java
@@ -4,9 +4,7 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.Claw;
 
 public class ToggleClaw extends InstantCommand {
